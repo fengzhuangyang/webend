ce6ce3fbe27877428427545900a78da2ee30c77c
fengzhuangyang <74775870+fengzhuangyang@users.noreply.github.com>
2020-11-23 15:33:09 +0800

    Update 112301.APP.java

--- 112301.APP.java
+++ 112301.APP.java
@@ -5,24 +5,17 @@ import java.util.Scanner;
 
 public class test {
 	public static void main(String[] args) {
-	int i=0;
-	String x[]=new String[26];
+
+
 	 Random random=new Random();
 	 Scanner scanner=new Scanner(System.in);
-		while(i<26){
-			x[i]=random.nextInt(100)+","+random.nextInt(100);
-			i++;
-		}
 		while(true){
-			while (true) {
-				String a=scanner.next();
-				char b=a.charAt(0);
-				if(Integer.valueOf(b)<123||Integer.valueOf(b)>97){
-					System.out.println(x[Integer.valueOf(b)-97]);
-					continue;
-				}
-				break;
-			}
+			int s=scanner.nextInt();
+			System.out.println(random.nextInt(100)+","+random.nextInt(100));
+		
+			
 		}
+	
+		
 	}
 }
