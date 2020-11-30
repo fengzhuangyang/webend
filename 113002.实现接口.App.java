1109aec400047b1d8837a8ae9cff62936fd84211
fengzhuangyang <74775870+fengzhuangyang@users.noreply.github.com>
2020-11-30 15:40:55 +0800

    Update 113002.实现接口.App.java

--- 113002.实现接口.App.java
+++ 113002.实现接口.App.java
@@ -5,6 +5,12 @@ public class App implements R {
 public static void main(String[] args) {
 	System.out.println(R.boy_filePath);
 	System.out.println(R.girl_filePath);
-	System.out.println(R.array2d[0][1]);
+	
+	
+	for (int row=0;R.array2d.length>row;row++) {
+		for (int colum=0;3>colum;colum++) {
+			System.out.println(R.array2d[row][colum]);
+		}
+	}
 }
 }
