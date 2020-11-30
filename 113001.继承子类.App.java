b7ce64d1432d212a3c446a927f75269bda5c9a29
fengzhuangyang <74775870+fengzhuangyang@users.noreply.github.com>
2020-11-30 15:04:42 +0800

    Create 113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,10 @@
+package bog;
+
+public class App extends R {
+
+public static void main(String[] args) {
+	System.out.println(R.boy_filePath);
+	System.out.println(R.girl_filePath);
+	System.out.println(R.array2d[0][1]);
+}
+}
