0de9aeff4f6b6a2ac7af5c91a5c54c38930ff75e
fengzhuangyang <74775870+fengzhuangyang@users.noreply.github.com>
2020-12-14 14:52:29 +0800

    Create 121401.奇数求和.App.java

---
+++ 121401.奇数求和.App.java
@@ -0,0 +1,13 @@
+package viour;
+
+public class java121401 {
+	public static void main(String[] args) {
+		long sum=0;
+		for(long i=1;i<2147483647;i=i+2){
+		    sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+	  
+
+}
